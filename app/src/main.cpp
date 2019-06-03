8f5d3317fb2481e8f0959d8aadcf98bf978bc898
Neil Burningham <neil.burningham@mediakind.com>
2019-06-03 08:00:35 +0100

    Use lib2 in app

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include "lib1-pub-header.h"
+#include "lib2-pub-header.h"
 
 int main(int, char **) {
 	std::cout << "1 + 2 = " << sum(1, 2) << '\n';
+    std::cout << "1 * 2 = " << mult(1, 2) << '\n';
 	return 0;
 }
